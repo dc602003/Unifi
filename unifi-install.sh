7c2bd243cf9f67986b526d1e52ab4735096a8729
dc602003 <dc602003@gmail.com>
2019-04-10 15:47:03 +0000

    Added new file with correct syntax. Veriried DC.

---
+++ unifi-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Ensure that the current session has sudo rights
+# sudo apt update
+# This command will add the Ubiquiti Unifi package sources into Apt
+echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' > /etc/apt/sources.list.d/100-ubnt-unifi.list
+# This section will add in the required packages for servers running 18.04 or aobve
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
+# Get the trusted GPG Keys
+wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
+# Install Apt HTTPS, and update
+apt update && apt install --yes apt-transport-https unifi
+# Thanks
+echo "All done. Have a nice day"
